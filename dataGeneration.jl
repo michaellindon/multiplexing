3e875697d46a055b9be44df70ecda6ead1f125ce
Michael Lindon <michael.s.lindon@gmail.com>
2015-09-24 15:00:43 -0400

    revised data generation

--- dataGeneration.jl
+++ dataGeneration.jl
@@ -4,11 +4,6 @@ f₁(x)=cos(2*(4*x-2))+2*exp(-(3^2)*(x-0.5).^2)
 λ₀(x)=λc*cdf(Normal(0,1),f₀(x))
 λ₁(x)=λc*cdf(Normal(0,1),f₁(x))
 δ=[0:0.01:1]
-#=plot(δ,f₁(δ),c="blue")=#
-#=plot(δ,f₂(δ),c="blue")=#
-#=plot(δ,λ₁(δ),c="blue")=#
-#=plot(δ,λ₂(δ),c="blue")=#
-#=GENERATE DATA=#
 
 srand(1)
 x=[0:0.01:1]
@@ -45,57 +40,97 @@ plot(δ,λs,c="purple")
 T=1
 nc=rand(Poisson(λc*T))
 tc=sort(rand(Uniform(0,T),nc))
-mppp0=Array(Float64,nc,7)
 #Times#Component#λ-thinned#α-thinned#λ-Z#α-Z
-mppp0[:,1]=tc
-mppp0[:,2]=zeros(nc)
+mppp0=DataFrame(t=tc,γ=zeros(Int64,nc),mf=zeros(Int64,nc),mg=Array(Int64,nc),Zf=Array(Float64,nc),Zg=Array(Float64,nc),label=Array(String,nc))
 for i=1:nc
-	mppp0[i,5]=rand(Normal(f₀(tc[i]),1))
-	if(mppp0[i,5]>0)
+	mppp0[i,:Zf]=rand(Normal(f₀(tc[i]),1))
+	if(mppp0[i,:Zf]>0)
 		#Accept
-		mppp0[i,3]=1.0
-		mppp0[i,6]=rand(Normal(gfun(tc[i]),1))
-		if(mppp0[i,6]>0)
-			mppp0[i,4]=1.0
+		mppp0[i,:mf]=1
+		mppp0[i,:Zg]=rand(Normal(gfun(tc[i]),1))
+		if(mppp0[i,:Zg]>0)
+			mppp0[i,:mg]=1
+			mppp0[i,:label]=string(mppp0[i,:γ],mppp0[i,:mf],mppp0[i,:mg])
 		else
-			mppp0[i,4]=0.0
+			mppp0[i,:mg]=0
+			mppp0[i,:label]=string(mppp0[i,:γ],mppp0[i,:mf],mppp0[i,:mg])
 		end
 	else 
 		#Thin
-		mppp0[i,3]=0.0
-		mppp0[i,4]=0.0
+		mppp0[i,:mf]=0
+		mppp0[i,:mg]=NA
+		mppp0[i,:Zg]=NA
+		mppp0[i,:label]=string(mppp0[i,:γ],mppp0[i,:mf],mppp0[i,:mg])
 	end
 end
+###Sanity Checks###
+if(!all(mppp0[mppp0[:label].=="00NA",:Zf].<0))
+	println("Error")
+end
+if(!all(mppp0[mppp0[:label].=="011",:Zf].>0))
+	println("Error")
+end
+if(!all(mppp0[mppp0[:label].=="011",:Zg].>0))
+	println("Error")
+end
+if(!all(mppp0[mppp0[:label].=="010",:Zf].>0))
+	println("Error")
+end
+if(!all(mppp0[mppp0[:label].=="010",:Zg].<0))
+	println("Error")
+end
+##################
+
 nc=rand(Poisson(λc*T))
 tc=sort(rand(Uniform(0,T),nc))
-mppp1=Array(Float64,nc,7)
 #Times#Component#λ-thinned#α-thinned#λ-Z#α-Z
-mppp1[:,1]=tc
-mppp1[:,2]=ones(nc)
+mppp1=DataFrame(t=tc,γ=ones(Int64,nc),mf=Array(Int64,nc),mg=Array(Int64,nc),Zf=Array(Float64,nc),Zg=Array(Float64,nc),label=Array(String,nc))
 for i=1:nc
-	mppp1[i,5]=rand(Normal(f₁(tc[i]),1))
-	if(mppp1[i,5]>0)
+	mppp1[i,:Zf]=rand(Normal(f₁(tc[i]),1))
+	if(mppp1[i,:Zf]>0)
 		#Accept
-		mppp1[i,3]=1.0
-		mppp1[i,6]=rand(Normal(gfun(tc[i]),1))
-		if(mppp1[i,6]<0)
-			mppp1[i,4]=1.0
+		mppp1[i,:mf]=1
+		mppp1[i,:Zg]=rand(Normal(gfun(tc[i]),1))
+		if(mppp1[i,:Zg]<0)
+			mppp1[i,:mg]=1
+			mppp1[i,:label]=string(mppp1[i,:γ],mppp1[i,:mf],mppp1[i,:mg])
 		else
-			mppp1[i,4]=0.0
+			mppp1[i,:mg]=0
+			mppp1[i,:label]=string(mppp1[i,:γ],mppp1[i,:mf],mppp1[i,:mg])
 		end
 	else 
 		#Thin
-		mppp1[i,3]=0.0
-		mppp1[i,4]=0.0
+		mppp1[i,:mf]=0
+		mppp1[i,:mg]=NA
+		mppp1[i,:Zg]=NA
+		mppp1[i,:label]=string(mppp1[i,:γ],mppp1[i,:mf],mppp1[i,:mg])
 	end
 end
+###Sanity Checks###
+if(!all(mppp1[mppp1[:label].=="10NA",:Zf].<0))
+	println("Error")
+end
+if(!all(mppp1[mppp1[:label].=="111",:Zf].>0))
+	println("Error")
+end
+if(!all(mppp1[mppp1[:label].=="111",:Zg].<0))
+	println("Error")
+end
+if(!all(mppp1[mppp1[:label].=="110",:Zf].>0))
+	println("Error")
+end
+if(!all(mppp1[mppp1[:label].=="110",:Zg].>0))
+	println("Error")
+end
+##################
 
 
 mppp=vcat(mppp0,mppp1)
-indices=sortperm(mppp[:,1])
+indices=sortperm(mppp[:,:t])
 mppp=mppp[indices,:]
-ix0=find((mppp[:,2].==0.0)&(mppp[:,4].==1.0))
-ix1=find((mppp[:,2].==1.0)&(mppp[:,4].==1.0))
+ix0=find((mppp[:,:γ].==0.0)&(mppp[:,:mg].==1.0))
+ix1=find((mppp[:,:γ].==1.0)&(mppp[:,:mg].==1.0))
+mppp[:g]=zeros(Float64,size(mppp,1))
 plot(mppp[ix0,1],-35*ones(length(ix0)),c="blue",marker="|",linestyle="None",markersize=10)
 plot(mppp[ix1,1],-35*ones(length(ix1)),c="red",marker="|",linestyle="None",markersize=10)
 subplot(212)
