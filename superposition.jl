2a994344a24bb47dc867345b982999d2984acb3d
Michael Lindon <michael.s.lindon@gmail.com>
2015-10-14 13:54:11 -0400

    refactor types

--- superposition.jl
+++ superposition.jl
@@ -1,27 +1,33 @@
-using Distributions
+importall Base.Random
+importall Distributions
+using PDMats
 using PyPlot
 
-include("functions.jl")
+include("spikeandslab.jl")
+include("poissonpointprocess.jl")
+include("gaussianprocess.jl")
 include("cleandataGeneration.jl")
 #=include("dataGeneration.jl")=#
 #=plot(grid,alpha,c="green")=#
 
-niter=10
+niter=20
 ZgTrace=Array(Float64,length(ξ),niter)
 gTrace=Array(Float64,length(ξ),niter)
 γTrace=Array(Float64,length(ξ),niter)
+gConditional=GP(g,σ²,ρ²,ψ²,"function")
 Ξₚ=PPProcess(Λ)
+σ²=1.0
 for iter=1:niter
-	Tₚ=realization(Ξₚ,0,Tobs)
-	gₚ=PredictGP(Tₚ,g,1.0,ρ²,ψ²,"function")
+	Tₚ=rand(Ξₚ,0,Tobs)
+	gₚ=rand(gConditional,Tₚ)
 	ξ₀₁₀=Dict{Float64,Dict{UTF8String,Float64}}()
 	for t in Tₚ
 		if(rand(Bernoulli((1-Φ(gₚ[t]))*λ₀(t)/Ξₚ.λ(t)))==1)
 			ξ₀₁₀[t]=Dict{UTF8String,Float64}("Zg"=>rand(Truncated(Normal(gₚ[t],1),-Inf,0)), "γ"=>0)
 		end
 	end
-	Tₚ=realization(Ξₚ,0,Tobs)
-	gₚ=PredictGP(Tₚ,g,1.0,ρ²,ψ²,"function")
+	Tₚ=rand(Ξₚ,0,Tobs)
+	gₚ=rand(gConditional,Tₚ)
 	ξ₁₁₀=Dict{Float64,Dict{UTF8String,Float64}}()
 	for t in Tₚ
 		if(rand(Bernoulli(Φ(gₚ[t])*λ₁(t)/Ξₚ.λ(t)))==1)
@@ -47,22 +53,24 @@ for iter=1:niter
 	for t in keys(ξ₀₁₀)
 		Zgc[t]=ξ₀₁₀[t]["Zg"]
 	end
-	g=PosteriorGP(Zgc,1.0,ρ²,ψ²)
-	gTrace[:,iter]=[g[t] for t in keys(ξ)]
-	ZgTrace[:,iter]=[ξ[key]["Zg"] for key in keys(ξ)]
-	γTrace[:,iter]=[ξ[key]["γ"] for key in keys(ξ)]
+	gProcess=GP(Zgc,σ²,ρ²,ψ²,"response")
+	g=rand(gProcess)
+	gConditional=GP(g,σ²,ρ²,ψ²,"function")
+	#=gTrace[:,iter]=[g[t] for t in keys(ξ)]=#
+	#=ZgTrace[:,iter]=[ξ[key]["Zg"] for key in keys(ξ)]=#
+	#=γTrace[:,iter]=[ξ[key]["γ"] for key in keys(ξ)]=#
 	plot(sort(collect(keys(g))),Φ([g[key] for key in sort(collect(keys(g)))]),c="grey",alpha=0.1);
 end
 
-tdataStream=open("tjuliaHigh.txt","w")
-gdataStream=open("gjuliaHigh.txt","w")
-gammadataStream=open("gammajuliaHigh.txt","w")
-zgdataStream=open("zgjuliaHigh.txt","w")
-writedlm(tdataStream,t)
-writedlm(gdataStream,gTrace)
-writedlm(zgdataStream,zgTrace)
-writedlm(gammadataStream,γTrace)
-close(tdataStream)
-close(gdataStream)
-close(zgdataStream)
-close(gammadataStream)
+#=tdataStream=open("tjuliaHigh.txt","w")=#
+#=gdataStream=open("gjuliaHigh.txt","w")=#
+#=gammadataStream=open("gammajuliaHigh.txt","w")=#
+#=zgdataStream=open("zgjuliaHigh.txt","w")=#
+#=writedlm(tdataStream,t)=#
+#=writedlm(gdataStream,gTrace)=#
+#=writedlm(zgdataStream,zgTrace)=#
+#=writedlm(gammadataStream,γTrace)=#
+#=close(tdataStream)=#
+#=close(gdataStream)=#
+#=close(zgdataStream)=#
+#=close(gammadataStream)=#
