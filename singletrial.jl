ce7e46aa1034c59c86fa121d5a6aeff0eb757156
Michael Lindon <michael.s.lindon@gmail.com>
2015-11-30 13:21:22 -0500

    appears to be working with statespace

--- singletrial.jl
+++ singletrial.jl
@@ -10,26 +10,27 @@ include("poissonpointprocess.jl")
 include("gaussianprocess.jl")
 
 
-srand(5)
-#=srand(4)=#
+srand(30)
 disc=0.01
 Tobs=1
 grid=collect(0:disc:Tobs)
 σ²=1.0
-ρ²=1.0
-ψ²=30.0
-#=g=rand(GP(Array(Float64,0),Array(Float64,0),σ²,ρ²,ψ²,"response"),grid)=#
+ł=0.1
+p=3
+ν=p+0.5
+λ=sqrt(2*ν)/ł
 g=chol(Kernel(grid,grid,ρ²,ψ²)+0.000000001*eye(length(grid)))'*rand(Normal(0,1),length(grid))
 gfun(arg)=g[maximum(find( y->(y <= arg), grid))]
-λ(x)=Λ*Φ(gfun(x))
-plot(grid,map(λ,grid),c="green")
+Λ=1000
+intensity(x)=Λ*Φ(gfun(x))
+plot(grid,map(intensity,grid),c="green")
 plot(grid,Λ*ones(length(grid)),c="black")
 nₚ=rand(Poisson(Λ*(Tobs)))
 Tₚ=sort(rand(Uniform(0,Tobs),nₚ))
 accepted=Array(Float64,0)
 rejected=Array(Float64,0)
 for t in Tₚ
-	if(rand(Bernoulli(λ(t)/Λ))==1)
+	if(rand(Bernoulli(intensity(t)/Λ))==1)
 		push!(accepted,t)
 	else
 		push!(rejected,t)
@@ -39,27 +40,28 @@ plot(Tₚ,-1*ones(length(Tₚ)),c="black",marker="|",linestyle="None",markersize
 plot(accepted,-0*ones(length(accepted)),c="green",marker="|",linestyle="None",markersize=20)
 plot(rejected,-140*ones(length(rejected)),c="red",marker="|",linestyle="None",markersize=20)
 
-niter=200
+niter=20
 ξ=Dict{Float64,Dict{UTF8String,Float64}}()
 for t in accepted
 	ξ[t]=Dict("Zg"=>rand(Truncated(Normal(0,1),0,Inf)),"γ"=>0)
 end
-g=Dict{Float64,Float64}(zip(collect(keys(ξ)),[0 for t in collect(keys(ξ))]))
-g=Dict(zip(grid,g))
-gConditional=GP(g,σ²,ρ²,ψ²,"function")
+g=Dict()
+for foo in collect(keys(ξ))
+	g[foo]=rand(4,1)
+end
 Ξₚ=PPProcess(Λ)
 σ²=1.0
 for iter=1:niter
 	Tₚ=rand(Ξₚ,0,Tobs)
-	gₚ=rand(gConditional,Tₚ)
+	gₚ=FFBS2(g,Tₚ,λ,q)
 	ξ₀₁₀=Dict{Float64,Dict{UTF8String,Float64}}()
 	for t in Tₚ
-		if(rand(Bernoulli((1-Φ(gₚ[t]))))==1)
-			ξ₀₁₀[t]=Dict{UTF8String,Float64}("Zg"=>rand(Truncated(Normal(gₚ[t],1),-Inf,0)), "γ"=>0)
+		if(rand(Bernoulli((1-Φ(gₚ[t][1]))))==1)
+			ξ₀₁₀[t]=Dict{UTF8String,Float64}("Zg"=>rand(Truncated(Normal(gₚ[t][1],1),-Inf,0)), "γ"=>0)
 		end
 	end
 	for t in keys(ξ)
-		ξ[t]["Zg"]=rand(Truncated(Normal(g[t],1),0,Inf))
+		ξ[t]["Zg"]=rand(Truncated(Normal(g[t][1],1),0,Inf))
 	end
 	Zgc=Dict{Float64,Float64}()
 	for t in keys(ξ)
@@ -68,8 +70,6 @@ for iter=1:niter
 	for t in keys(ξ₀₁₀)
 		Zgc[t]=ξ₀₁₀[t]["Zg"]
 	end
-	gProc=GP(Zgc,σ²,ρ²,ψ²,"response")
-	g=rand(gProc)
-	gConditional=GP(g,σ²,ρ²,ψ²,"function")
-	plot(sort(collect(keys(g))),Λ*Φ([g[key] for key in sort(collect(keys(g)))]),c="grey",alpha=0.1);
+	g=FFBS(Zgc,collect(keys(Zgc)),σ²,λ,q)
+	plot(sort(collect(keys(g))),Λ*Φ([g[key][1] for key in sort(collect(keys(g)))]),c="grey",alpha=0.1);
 end
